bfee2c5919c07216c39acf3d551d95c9163a187d
SamJulia <skotarjulia@gmail.com>
2020-06-13 17:11:19 -0700

    one more tc and fixes

--- test/specs/LowerLimitField.spec.js
+++ test/specs/LowerLimitField.spec.js
@@ -4,13 +4,13 @@ const changeStepOptionBtnRight = '//button[@name="positive"]';
 const lowerLimitField = '//input[@name="lower"]';
 const upperLimitField = '//input[@name="upper"]';
 
-describe('Test Lower Limit Field(LLF', () => {
+describe('Test Lower Limit Field(LLF)', () => {
 
     before(function () {
         browser.url(homeUrl);
     });
 
-    it('should have value 1 in LLF when LLF is active', () => {changeStepOptionBtnLeft
+    it('should have value 1 in LLF when LLF is active', () => {
         $(changeStepOptionBtnLeft).click();
         expect($(lowerLimitField)).toHaveValue('1');
     });
@@ -24,23 +24,30 @@ describe('Test Lower Limit Field(LLF', () => {
         expect($(changeStepOptionBtnLeft)).toHaveText('CHANGE STEP OPTIONS?')
     });
 
-    it("should changed default value from 1 to 2 after LLF is active", () => {
+    it("should change the default value from 1 to 2", () => {
         $(changeStepOptionBtnLeft).click();
-        $(lowerLimitField).setValue('2');
+        $(lowerLimitField).clearValue();
+        $(lowerLimitField).addValue('2');
+        expect($(lowerLimitField)).toHaveValue('2')
     });
 
     it("LLF should accept 9 if ULF is 9", () => {
         $(changeStepOptionBtnRight).click();
         $(upperLimitField).setValue('9')
-        $(lowerLimitField).setValue('9');
-        browser.keys('Back space')
+        $(lowerLimitField).clearValue();
+        $(lowerLimitField).addValue('9');
+        expect($(lowerLimitField)).toHaveValue('9')
     });
 
-    it("should have error msg 'ERROR: Input must be an Integer' with empty input LLF",  () => {
+    it("should have error msg 'ERROR: Input must be an Integer' with empty input LLF", () => {
+        browser.keys('Back space')
         const errorMsgWithNoInput = $("//div[contains(@class, 'alert-danger')]/span");
         expect(errorMsgWithNoInput).toHaveText("ERROR: Input must be an INTEGER")
     });
+
+    it("should have empty LLF with letters input ('a')", () => {
+        $(lowerLimitField).keys('a');
+        expect($(lowerLimitField)).toHaveValue('');
+    });
 });
-
-
 
