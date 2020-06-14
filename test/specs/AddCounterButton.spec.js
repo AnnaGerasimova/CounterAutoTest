ef25b397244cb251d9fe07be9c467a8abccbd370
olgashchekina <olya6avg@gmail.com>
2020-06-14 14:50:38 -0700

    tests changes

--- test/specs/AddCounterButton.spec.js
+++ test/specs/AddCounterButton.spec.js
@@ -1,36 +1,40 @@
+const homeUrl = 'https://likejean.github.io/homework-5/';
 const InitialValue = '[data-testid="counter-value-input"]';
 const CounterTitle = '[data-testid="counter-name-input"]';
 const btn = '.btn-success';
 
 
-
 describe('Add counter button tests', () => {
   before('open counter app', () => {
-    browser.url('https://likejean.github.io/homework-5/');
+    browser.url(homeUrl);
   });
 
   it('should verify that Add Counter Button is disabled with empty Add name field', () => {
     $(CounterTitle).setValue(' ');
     $(CounterTitle).setValue('');
-    expect (($(btn)).isEnabled()).toBe(false)
+    expect ($(btn)).toBeDisabled();
   })
 
-  // it('should verify that Add Counter Button is disabled with empty Default value field', (qualifiedName, value) => {
-  //   browser.refresh()
-  //   // $(InitialValue).getValue().length;
-  //   // for(let i = 0; i < InitialValue; i++){
-  //   //   $(InitialValue).keys('');
-  //   // }
-  //   $(InitialValue).clearValue()
-  //   expect ($(btn).isEnabled()).toBe(false);
-  // })
+  it('should verify that Add Counter Button is disabled with empty Default value field', () => {
+    browser.refresh()
+    $(InitialValue).click();
+    $(InitialValue).getValue();
+    for(let i = 0; i < InitialValue.length; i++){
+        browser.keys('Back space');
+      }
+    expect ($(btn)).toBeDisabled();
+  })
 
   it('should verify that Add Counter Button is disabled with empty Add name field and Default value field', () => {
     browser.refresh()
     $(CounterTitle).setValue(' ');
     $(CounterTitle).setValue('');
-    $(InitialValue).clearValue()
-    expect ($(btn).isEnabled()).toBe(false);
+    $(InitialValue).click();
+    $(InitialValue).getValue();
+    for(let i = 0; i < InitialValue.length; i++){
+      browser.keys('Back space');
+    }
+    expect ($(btn)).toBeDisabled();
   })
 
   it('should Verify that Add Counter Button is disabled when Default value field  = float number && Add name ' +
@@ -38,7 +42,7 @@ describe('Add counter button tests', () => {
     browser.refresh()
     $(CounterTitle).setValue('Hello!');
     $(InitialValue).setValue(1.4)
-    expect ($(btn).isEnabled()).toBe(false);
+    expect ($(btn)).toBeDisabled();
   })
 
   it('should Verify that Add Counter Button is disabled when Default value field = 50  && Add name ' +
@@ -46,30 +50,34 @@ describe('Add counter button tests', () => {
     browser.refresh()
     $(CounterTitle).setValue('@');
     $(InitialValue).setValue(50)
-    expect ($(btn).isEnabled()).toBe(false);
+    expect ($(btn)).toBeDisabled();
   })
 
-  // it('should Verify that Add Counter Button is disabled when Default value field = "E"  && Add name field = "Counter 4"', () => {
-  //   browser.refresh()
-  //   $(CounterTitle).setValue('Counter 4');
-  //   $(InitialValue).setValue('E')
-  //   expect ($(btn).isEnabled()).toBe(false);
-  //   browser.pause(1000);
-  // }) // не принимает не цифры
-  //
+  it('should Verify that Add Counter Button is disabled when Default value field = "E"  && Add name field = "Counter 4"', () => {
+    browser.refresh()
+    $(CounterTitle).setValue('Counter 4');
+    $(InitialValue).click();
+    $(InitialValue).getValue();
+    for(let i = 0; i < InitialValue.length; i++){
+      browser.keys('Back space');
+    }
+    $(InitialValue).setValue('E')
+    expect ($(btn)).toBeDisabled();
+    browser.pause(1000);
+  })
+
   it('should Verify that Add Counter Button is disabled when Default value field = "60"  && Add name field accept value ' +
     'less then 7 chars ("123456")', () => {
     browser.refresh()
     $(CounterTitle).setValue('123456');
     $(InitialValue).setValue(60)
-    expect ($(btn)).not.toBeEnabled();
+    expect ($(btn)).toBeDisabled();
   })
 
   it('should Verify "Add counter" button active if Default value field and Add name field are filled with default ' +
     'data (50, Counter name)', () => {
     browser.refresh()
-    const btn = browser.$('[class="btn-success btn Ripple-parent add col-6"]')
-    expect ($(btn).isEnabled() && $(btn).isClickable()).toBe(true);
+    expect ($(btn)).toBeEnabled();
   })
 
   it('should Verify "Add counter" button active if Default value field = 100 and Add name field ' +
@@ -77,14 +85,14 @@ describe('Add counter button tests', () => {
     browser.refresh()
     $(InitialValue).setValue(100);
     $(CounterTitle).setValue('Counter 4');
-    expect ($(btn).isEnabled() && $(btn).isClickable()).toBe(true);
+    expect ($(btn)).toBeEnabled();
   })
 
   it('should Verify that new counter will appear after Add Counter Button is clicked with default fields', () => {
     browser.refresh()
     $(btn).click();
-    const NewCounter = (browser.$('[class="container-fluid counter-wrapper"]'))
-    expect (NewCounter.isDisplayed()).toBe(true);
+    const NewCounter = browser.$('[class="container-fluid counter-wrapper"]')
+    expect (NewCounter).toBeDisplayed();
   })
 
   it('should Verify that after a new counter has been added, its order number  = previous counters order number + 1 ', () => {
