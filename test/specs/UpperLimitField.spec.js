e060d4dbfaf220a7510bdec2d0d9384ba2c6885b
Anna Gerasimova <gerasimova.hanna@gmail.com>
2020-06-14 13:43:29 -0700

    auto-tests for ULF part1

---
+++ test/specs/UpperLimitField.spec.js
@@ -0,0 +1,36 @@
+const homeUrl = 'https://likejean.github.io/homework-5/';
+const upperLimitField = '//button[@name="positive"]';
+const upperLimitFieldActive = '//input[@name="upper"]';
+const deleteUlF = '//button[@class="btn-danger btn-outline-danger btn Ripple-parent close-button"]';
+
+
+describe('Upper Limit Field', () => {
+    before(function () {
+        browser.url(homeUrl);
+    });
+
+    it("ULF should have default value = 3, after ULF is active (clicked on placeholder)", () => {
+        $(upperLimitField).click();
+        expect($(upperLimitFieldActive)).toHaveValue('3')
+    });
+
+    it("should Delete X button should appears when ULF is active (on ULF left side)", () => {
+        expect($(deleteUlF)).toBePresent();
+    });
+
+    it("Delete X button should disappear, after Delete X button is clicked ", () => {
+        $(deleteUlF).click();
+        expect($(deleteUlF).isDisplayed()).toBe(false);
+    });
+
+    it("should appears placeholder = 'Change Step Options?', after Delete X button is clicked, ", () => {
+        // $(deleteUlF).click();
+        expect($(upperLimitField)).toHaveText('CHANGE STEP OPTIONS?')
+    });
+
+    it("ULF field should be cleared, after Delete X button is clicked.", () => {
+        $(upperLimitField).click();
+        expect($(upperLimitField)).toHaveValue('');
+    });
+
+})
