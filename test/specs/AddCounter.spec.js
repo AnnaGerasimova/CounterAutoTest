1ebc698ab8422a6006859cc3ebeb7d324b3e7666
GalinaRuban <galina.ruban11@gmail.com>
2020-06-15 16:23:01 -0400

    all const are lowerCase, two const moved before describe, 4 tests are bug, thus, falling

--- test/specs/AddCounter.spec.js
+++ test/specs/AddCounter.spec.js
@@ -1,85 +1,72 @@
 const homeUrl = "https://likejean.github.io/homework-5/";
-// const DefaultValueField = '//input[@data-testid= "counter-value-input"]'
-// not working when I put this const at the top with $() or without
+const defaultValueField = '//input[@data-testid= "counter-value-input"]'
+const error = '//div[@role= "alert"]'
 
 describe("Add counter", () => {
     before(() => {
         browser.url(homeUrl);
     });
 
-it("verify that Default Value Field accepts 1",  () => {
-     const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-     DefaultValueField.setValue('1');
-     const Error = $('//div[@role= "alert"]')
-     expect((Error).isDisplayed()).toBe(false);
-     DefaultValueField.clearValue();
-});
+    it("verify that Default Value Field accepts 1", () => {
+        $(defaultValueField).setValue('1');
+        expect($(error).isDisplayed()).toEqual(false);
+        $(defaultValueField).clearValue();
+    });
 
-    it("verify that Default Value Field accepts 1000000",  () => {
-        const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-        DefaultValueField.setValue('1000000');
-        const Error = $('//div[@role= "alert"]');
-        expect((Error).isDisplayed()).toBe(false);
-        DefaultValueField.clearValue();
-        browser.refresh();
+    it("verify that Default Value Field accepts 1000000", () => {
+        $(defaultValueField).setValue('1000000');
+        expect($(error).isDisplayed()).toEqual(false);
+        $(defaultValueField).clearValue();
+
 
     });
 
-// // test is failed since the app started to accept floating numbers, no error that was recorded earlier
+// test failed since the app started to accept floating numbers, no error that was recorded earlier
 
-    it("verify that Verify that Default Value Field doesn't accept floating numbers",  () => {
-        const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-        DefaultValueField.clearValue();
-        DefaultValueField.moveTo()
-        DefaultValueField.setValue('1.3');
-        const Error = $('//div[@role= "alert"]')
-        expect((Error).isDisplayed()).toBe(true);
+    it("verify that Verify that Default Value Field doesn't accept floating numbers", () => {
+        $(defaultValueField).moveTo();
+        $(defaultValueField).setValue('1.3');
+        expect($(error).isDisplayed()).toEqual(true);
+
     });
 
     //test if we expect an error, falling since no error
-    it("verify that Default Value Field doesn't accept special characters and error msg is displayed",  () => {
-        const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-        DefaultValueField.clearValue()
-        $(DefaultValueField).moveTo()
-        DefaultValueField.setValue('@');
-        const Error = $('//div[@role= "alert"]')
-        expect((Error).isDisplayed()).toBe(true);
-        DefaultValueField.clearValue()
+
+    it("verify that Default Value Field doesn't accept special characters and error msg is displayed", () => {
+        $(defaultValueField).moveTo();
+        $(defaultValueField).setValue('@');
+        expect($(error).isDisplayed()).toEqual(true);
+        $(defaultValueField).clearValue();
     });
 
-//     //test if we expect a field to stay empty
+    //test if we expect a field to stay empty
 
-    it("verify that Default Value Field doesn't accept special characters and field stays empty",  () => {
-        const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-        $(DefaultValueField).moveTo()
-         DefaultValueField.setValue('qa');
-        const textInsideInputBox = DefaultValueField.getValue();
+    it("verify that Default Value Field doesn't accept special characters and field stays empty", () => {
+        $(defaultValueField).moveTo()
+        $(defaultValueField).setValue('qa');
+        const textInsideInputBox = $(defaultValueField).getValue();
         expect(textInsideInputBox).toEqual('');
-        console.log('--------------------------')
-        console.log("Input field is empty");
+
 
     });
 
-// // it's bug, thus, it fails
+// it's bug, thus, it fails
 
-    it("verify that when initial value '50' is deleted in the Default Value field, it stays empty",  () => {
-        const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-        DefaultValueField.setValue('67');
-        const inputValue = DefaultValueField.getValue();
-        for(let i = 0; i < inputValue.length; i++) {
+    it("verify that when initial value '50' is deleted in the Default Value field, it stays empty", () => {
+        $(defaultValueField).setValue('67');
+        const inputValue = $(defaultValueField).getValue();
+        for (let i = 0; i < inputValue.length; i++) {
             browser.keys('Back space');
-            browser.pause(2000)
+
         }
-        const Error = $('//div[@role= "alert"]')
-        expect((Error).isDisplayed()).toBe(false);
+        expect($(error).isDisplayed()).toEqual(false);
 
     });
 
-    it("verify that Default Value Field accepts -10",  () => {
-        const DefaultValueField = $('//input[@data-testid= "counter-value-input"]')
-        DefaultValueField.setValue('-10');
-        const Error = $('//div[@role= "alert"]')
-        expect((Error).isDisplayed()).toBe(false);
+    it("verify that Default Value Field accepts -10", () => {
+        $(defaultValueField).setValue('-10');
+        expect($(error).isDisplayed()).toEqual(false);
+
     });
 
 
