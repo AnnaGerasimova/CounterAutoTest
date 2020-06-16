f62869c0a2fea38d3cb3de1c50c3b480e820991c
SamJulia <skotarjulia@gmail.com>
2020-06-15 21:09:58 -0700

    addCount

--- test/specs/AddCounter.spec.js
+++ test/specs/AddCounter.spec.js
@@ -1,12 +1,11 @@
 const homeUrl = "https://likejean.github.io/homework-5/";
-const defaultValueField = '//input[@data-testid= "counter-value-input"]'
-const error = '//div[@role= "alert"]'
+const defaultValueField = '//input[@data-testid= "counter-value-input"]';
+const error = '//div[@role= "alert"]';
+const addBtnCounter = '//input[@name="name"]';
 
 describe("Add counter", () => {
-
     before('open', () => {
         browser.url(homeUrl);
-
     });
 
     it("verify that Default Value Field accepts 1", () => {
@@ -19,7 +18,6 @@ describe("Add counter", () => {
         $(defaultValueField).setValue('1000000');
         expect($(error).isDisplayed()).toEqual(false);
         $(defaultValueField).clearValue();
-
     });
 
 // test failed since the app started to accept floating numbers, no error that was recorded earlier
@@ -28,7 +26,6 @@ describe("Add counter", () => {
         $(defaultValueField).moveTo();
         $(defaultValueField).setValue('1.3');
         expect($(error).isDisplayed()).toEqual(true);
-
     });
 
     //test if we expect an error, falling since no error
@@ -47,7 +44,6 @@ describe("Add counter", () => {
         $(defaultValueField).setValue('qa');
         const textInsideInputBox = $(defaultValueField).getValue();
         expect(textInsideInputBox).toEqual('');
-
     });
 
 // it's bug, thus, it fails
@@ -59,17 +55,47 @@ describe("Add counter", () => {
             browser.keys('Back space');
         }
         expect($(error).isDisplayed()).toEqual(false);
-
     });
 
     it("verify that Default Value Field accepts -10", () => {
         $(defaultValueField).setValue('-10');
         expect($(error).isDisplayed()).toEqual(false);
-
     });
 
+    it("should accept integer", () => {
+        $(addBtnCounter).setValue('2000001');
+        expect($(error).isDisplayed()).toEqual(false);
+    });
+
+    it("should Error Message is displayed if value length is less then 7 ", () => {
+        $(addBtnCounter).setValue('Gj#js$');
+        expect($(error).isDisplayed()).toEqual(true);
+    });
+
+    it("should accept value with length is less then 7", () => {
+        $(addBtnCounter).setValue('Gj#js$h');
+        expect($(error).isDisplayed()).toEqual(false);
+    });
+
+    it("should accept alphabet letters in Upper Case length is less then 7", () => {
+        $(addBtnCounter).setValue('ABCDEFJ');
+        expect($(error).isDisplayed()).toEqual(false);
+    });
+
+    it("should accept alphabet letters in Lower Case length is less then 7", () => {
+        $(addBtnCounter).setValue('alicemo');
+        expect($(error).isDisplayed()).toEqual(false);
+    });
+
+    it("should accept special length is less then 7", () => {
+        $(addBtnCounter).setValue('@!@#$%&');
+        expect($(error).isDisplayed()).toEqual(false);
+    });
+
+    it("should accept floating numbers length is less then 7", () => {
+        $(addBtnCounter).setValue('3456.145');
+        expect($(error).isDisplayed()).toEqual(false);
+    });
 });
-
-
 
 
