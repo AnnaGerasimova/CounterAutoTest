3a7cbc05d350bcb3f6686aa1525d56d5025aa31c
AygulAkhmadeeva <aygulaaa@gmail.com>
2020-06-15 20:03:08 -0700

    addCounter (2)

--- test/specs/AddCounter.spec.js
+++ test/specs/AddCounter.spec.js
@@ -1,73 +1,46 @@
 const homeUrl = "https://likejean.github.io/homework-5/";
-const defaultValueField = '//input[@data-testid= "counter-value-input"]'
 const error = '//div[@role= "alert"]'
+const addBtnCounter = '//input[@name="name"]'
 
-describe("Add counter", () => {
-
-    before('open', () => {
-        browser.url(homeUrl);
-
+describe('ADD BUTTONS COUNTER', () => {
+    before('open a page', () => {
+        browser.url(homeUrl)
     });
 
-    it("verify that Default Value Field accepts 1", () => {
-        $(defaultValueField).setValue('1');
-        expect($(error).isDisplayed()).toEqual(false);
-        $(defaultValueField).clearValue();
+    it("should accept integer", () => {
+        $(addBtnCounter).setValue('2000001');
+        expect($(error).isDisplayed()).false;
     });
 
-    it("verify that Default Value Field accepts 1000000", () => {
-        $(defaultValueField).setValue('1000000');
-        expect($(error).isDisplayed()).toEqual(false);
-        $(defaultValueField).clearValue();
-
+    it("should Error Message is displayed if value length is less then 7 characters", () => {
+        $(addBtnCounter).setValue('Gj#js$');
+        expect($(error).isDisplayed()).true;
     });
 
-// test failed since the app started to accept floating numbers, no error that was recorded earlier
-
-    it("verify that Verify that Default Value Field doesn't accept floating numbers", () => {
-        $(defaultValueField).moveTo();
-        $(defaultValueField).setValue('1.3');
-        expect($(error).isDisplayed()).toEqual(true);
-
+    it("should accept value with length >=7 characters", () => {
+        $(addBtnCounter).setValue('Gj#js$h');
+        expect($(error).isDisplayed()).false;
     });
 
-    //test if we expect an error, falling since no error
-
-    it("verify that Default Value Field doesn't accept special characters and error msg is displayed", () => {
-        $(defaultValueField).moveTo();
-        $(defaultValueField).setValue('@');
-        expect($(error).isDisplayed()).toEqual(true);
-        $(defaultValueField).clearValue();
+    it("should accept alphabet letters in Upper Case >= 7 characters", () => {
+        $(addBtnCounter).setValue('ABCDEFJ');
+        expect($(error).isDisplayed()).false;
     });
 
-    //test if we expect a field to stay empty
-
-    it("verify that Default Value Field doesn't accept special characters and field stays empty", () => {
-        $(defaultValueField).moveTo();
-        $(defaultValueField).setValue('qa');
-        const textInsideInputBox = $(defaultValueField).getValue();
-        expect(textInsideInputBox).toEqual('');
-
+    it("should accept alphabet letters in Lower Case >= 7 characters", () => {
+        $(addBtnCounter).setValue('alicemo');
+        expect($(error).isDisplayed()).false;
     });
 
-// it's bug, thus, it fails
-
-    it("verify that when initial value '50' is deleted in the Default Value field, it stays empty", () => {
-        $(defaultValueField).setValue('67');
-        const inputValue = $(defaultValueField).getValue();
-        for (let i = 0; i < inputValue.length; i++) {
-            browser.keys('Back space');
-        }
-        expect($(error).isDisplayed()).toEqual(false);
-
+    it("should accept special >= 7 characters", () => {
+        $(addBtnCounter).setValue('@!@#$%&');
+        expect($(error).isDisplayed()).false;
     });
 
-    it("verify that Default Value Field accepts -10", () => {
-        $(defaultValueField).setValue('-10');
-        expect($(error).isDisplayed()).toEqual(false);
-
+    it("should accept floating numbers >= 7 characters", () => {
+        $(addBtnCounter).setValue('3456.145');
+        expect($(error).isDisplayed()).false;
     });
-
 });
 
 
